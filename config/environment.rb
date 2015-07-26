ed433025a836c0a01e97eb672b06e2870ccffc66
Yev <tangeroknight@gmail.com>
2015-07-26 14:55:07 -0500

    code cleanup configured log4r

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require 'log4r'
+require 'log4r/yamlconfigurator'
+
+Log4r::YamlConfigurator.load_yaml_file(File.expand_path('../log4r.yml', __FILE__))
 
 # Initialize the Rails application.
 Rails.application.initialize!
