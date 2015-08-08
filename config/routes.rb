4c70e6841c5e4903faef2233fdb372f9cd4c0fe9
Yev <tangeroknight@gmail.com>
2015-08-08 13:40:39 -0500

    removed email routes. Will not be used for the mobile app

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,4 @@ Rails.application.routes.draw do
   resources :listeners, except: [:new, :edit]
 
   post '/artists/:artist_name/songs/:song_name/request', to: 'listener_requests#request_song'
-
-  get AppConfig::WebSettings.email_song_request_route, 
-  to: 'emails#generate_song_request'
-   
-  get AppConfig::WebSettings.email_song_approve_route, 
-  to: 'emails#generate_song_approve'
-  
-  get AppConfig::WebSettings.email_cancel_reservation_route, 
-  to: 'emails#generate_cancel_reservation'
-  
-  get AppConfig::WebSettings.email_update_reservation_route, 
-  to: 'emails#generate_update_reservation'
 end
