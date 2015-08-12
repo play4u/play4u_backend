cd799ef9699d4dc039ffddb01176194eec56fd9e
Yev <tangeroknight@gmail.com>
2015-08-12 18:18:37 -0500

    fix loading path

--- config/initializers/thread_pool_init.rb
+++ config/initializers/thread_pool_init.rb
@@ -2,6 +2,7 @@ require 'thread/pool'
 require 'thread/channel'
 require 'thread/pipe'
 require 'thread/promise'
+require "#{Rails.root}/app/app_config/scalability_settings"
 
 module AppConfig
   module Scalability
